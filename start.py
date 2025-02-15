f2aa322b49a266dec04676ce59c0324f940000d5
YaRo <roman.yatsenko@hneu.net>
2025-02-15 16:19:19 +0200

    Add class AlienOnvasion

---
+++ start.py
@@ -0,0 +1,31 @@
+import sys
+
+import pygame
+
+
+class AlienInvasion:
+    """Клас для управління ресурсами та поведінкою гри"""
+
+    def __init__(self):
+        """Ініціалізує гру та створює ігрові ресурси"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Запуск основного циклу гри"""
+        while True:
+            # Відслідковування подій клавіатури та миші
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Відображення останнього прорисованого екрану
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    # Створення екземпляру та запуск гри
+    ai = AlienInvasion()
+    ai.run_game()
