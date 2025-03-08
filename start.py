10279fb61b63daaae4b5f615a1120b5ba5367597
YaRo <roman.yatsenko@hneu.net>
2025-03-08 16:21:30 +0200

    Refactor _create_fleet

--- start.py
+++ start.py
@@ -56,6 +56,14 @@ class AlienInvasion:
         elif event.key == pg.K_LEFT:
             self.ship.moving_left = False
 
+    def _create_alien(self, alien_number):
+        """Створює одного прибульця і розміщує його в ряду"""
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        self.aliens.add(alien)
+
     def _create_fleet(self):
         """Створює флот прибульців"""
         # Створення прибульця і визначення кількості прибульців в ряду
@@ -66,10 +74,7 @@ class AlienInvasion:
 
         # Створення першого ряду прибульців
         for alien_number in range(number_aliens_x):
-            alien = Alien(self)
-            alien.x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+            self._create_alien(alien_number)
 
     def _fire_bullet(self):
         """Створює новий снаряд та додає його до групи bullets"""
