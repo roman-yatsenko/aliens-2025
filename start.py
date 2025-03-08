a7b4f94231c8416098f6e16d393055dd1d68e0d1
YaRo <roman.yatsenko@hneu.net>
2025-03-08 16:34:14 +0200

    Add alien fleet

--- start.py
+++ start.py
@@ -56,25 +56,35 @@ class AlienInvasion:
         elif event.key == pg.K_LEFT:
             self.ship.moving_left = False
 
-    def _create_alien(self, alien_number):
+    def _create_alien(self, alien_number, row_number):
         """Створює одного прибульця і розміщує його в ряду"""
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien_height + 2 * alien_height * row_number
         self.aliens.add(alien)
 
     def _create_fleet(self):
         """Створює флот прибульців"""
         # Створення прибульця і визначення кількості прибульців в ряду
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - 2 * alien_width
         number_aliens_x = available_space_x // (2 * alien_width)
 
-        # Створення першого ряду прибульців
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
+        # Визначення кількості рядів
+        ship_height = self.ship.rect.height
+        available_space_y = (
+            self.settings.screen_height - (3 * alien_height) - ship_height
+        )
+        number_rows = available_space_y // (2 * alien_height)
+
+        # Створення флоту прибульців
+        for row_number in range(number_rows):
+            # Створення ряду прибульців
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
 
     def _fire_bullet(self):
         """Створює новий снаряд та додає його до групи bullets"""
